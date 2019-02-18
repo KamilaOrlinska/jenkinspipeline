574ad23c311853afa5c4dfaf36632270b9245a36
KamilaOrlinska <kamila.orlinska@icloud.com>
2019-02-18 12:28:51 +0100

    remove files pushed by mistake

--- homebrew/Library/Homebrew/extend/os/linux/tap.rb
+++
@@ -1,7 +0,0 @@
-class CoreTap < Tap
-  # @private
-  def initialize
-    super "Homebrew", "core"
-    @full_name = "Linuxbrew/homebrew-core" unless ENV["HOMEBREW_FORCE_HOMEBREW_ON_LINUX"]
-  end
-end
